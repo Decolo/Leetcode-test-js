e7bed96a5725f84a6b7928fdd4c473b071fa0976
Decolo <decolo@163.com>
2017-12-02 17:45:27 +0800

    'merge two sorted list'

--- MergeTwoSortedLists/README.md
+++ MergeTwoSortedLists/README.md
@@ -1,1 +1,21 @@
+### Desciption: ###
+
 Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists
+
+### Example: ###
+```
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+```
+
+### Recursive: ###
+
+1. Check the edge cases. If either l1 or l2 was null, return non-null list.
+2. Judge which of l1 or l2 was the smaller one. Recursively set l1/l2's next value point to next merge's result,
+meanwhile return l1/l2. The recursion will stop util one argument of recursive function becoming null.
+
+
+### Iteration: ###
+1. Set a head and pre as pointers. Head will be a node whose val is -1
+2. Every Step of Iteration, there are two list waiting for compare. The pointer pre will point to the smaller node.
+And one of the two lists will start from the smaller node's next to generate a new list for next ineration.
--- MergeTwoSortedLists/index.js
+++ MergeTwoSortedLists/index.js
@@ -10,9 +10,48 @@ function ListNode(val) {
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function(l1, l2) {
+
+// Recusive
+
+var mergeTwoLists1 = function(l1, l2) {
+  if (l1 === null) return l2
+  if (l2 === null) return l1
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists1(l1.next, l2)
+    return l1
+  } else {
+    l2.next = mergeTwoLists1(l2.next, l1)
+    return l2
+  }
+}
+
+// Iteration
+var mergeTwoLists2 = function(l1, l2) {
+  var pre, head 
+  head = pre = new ListNode(-1)
+  while (l1 !== null && l2 !== null) {
+    if (l1.val <= l2.val) {
+      pre.next = l1
+      l1 = l1.next
+    } else {
+      pre.next = l2
+      l2 = l2.next
+    }
+    pre = pre.next
+  }
+  pre.next = l1 === null ? l2 : l1
+  return head.next
+}
+
+// return a new ListNode instance
+var mergeTwoLists3 = function(l1, l2) {
   var newList, current
-  if (l1 === null || l2 === null) return 
+  if (l1 === null) {
+    return l2
+  }
+  if (l2 === null) {
+    return l1
+  } 
   if (l1.val < l2.val) {
     newList = current = new ListNode(l1.val)
     l1 = l1.next
@@ -37,4 +76,6 @@ var mergeTwoLists = function(l1, l2) {
   }
   return newList
 }
+
+
 
