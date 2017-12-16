a579611b32bffa086b5c9c9eebd70f97c53b0ed2
Decolo <decolo@163.com>
2017-12-16 15:38:07 +0800

    'mergeBinaryTree fix'

--- MergeTwoBinaryTrees/index.js
+++ MergeTwoBinaryTrees/index.js
@@ -1,8 +1,10 @@
 
 // Definition for a binary tree node.
-function TreeNode(val) {
-    this.val = val;
-    this.left = this.right = null;
+class TreeNode {
+  constructor(val) {
+    this.val =val
+    this.left = this.right = null
+  }
 }
 /**
  * @param {TreeNode} t1
@@ -10,77 +12,115 @@ function TreeNode(val) {
  * @return {TreeNode}
  */
 
+// merge the values of t2 and t1 to obtain a new tree（得到一个新树)
+// iteration
 
 
-let mergeTree2 = (t1, t2) => {
-  if (t2 === null || t1 === null) {
+
+// recursion 
+// let mergeToNewTree = (t1, t2) => {
+//   // init
+//   if (t1 === null && t2 === null) {
+//     return 
+//   } else if (t1 === null && t2 !== null) {
+//     return new TreeNode(t2.val)
+//   } else if (t1 !== null && t2 === null) {
+//     return new TreeNode(t1.val)
+//   } else {
+//     let queue1 = [[t1, t2]]
+//     let queue2 = []
+//     let tempArr
+//     while (queue1.length !== 0) {
+//       tempArr = queue1.shift()
+      
+//       if (tempArr[0] !== null && tempArr[1] !== null) {
+//         queue2.push(tempArr[0].val + tempArr[1].val)
+//         if (tempArr[0].left !== null || tempArr[1].left !== null) {
+//           queue1.push([tempArr[0].left, tempArr[1].left])
+//         }
+//         if (tempArr[0].right !== null || tempArr[1].right !== null) {
+//           queue1.push([tempArr[0].right, tempArr[1].right])
+//         }
+//       }
+//     }
+//     return t3
+//   }
+// }
+
+
+
+
+
+
+
+// merge t2 into t1 将t2的节点都合并到t1上
+// recursion
+let mergeTrees1 = (t1, t2) => {
+  if (t1 === null && t2 === null) {
+    return null
+  } else if (t2 !== null && t1 === null) {
+    return new TreeNode(t2.val)
+  } else if (t1 !== null && t2 === null) {
+    return  t1
+  } else {
+    t1.val += t2.val
+    t1.left = mergeTrees1(t1.left, t2.left)
+    t1.right = mergeTrees1(t1.right, t2.right)
+    return t1
+  }
+}
+
+// iteration
+let mergeTrees2 = (t1, t2) => {
+  if (t1 === null) {
     return
   }
-  let queue = [], tempArr
-  queue.push([t1, t2])
-  while (stack.length !== 0) {
+  let queue = [[t1, t2]], tempArr
+  while (queue.length !== 0) {
     tempArr = queue.shift()
-    if (tempArr[0] === null && tempArr[1] === null) {
+    if (tempArr[1] === null) {
       continue
     } else {
-      if (tempArr[0].left === null) {
-        tempArr[0].left = tempArr[1].left
-      } else {
-        stack.push([tempArr[0].left, tempArr[1].left])
-      }
-      if (tempArr[0].right === null) {
-        tempArr[0].right = tempArr[1].right
-      } else {
-        stack.push([tempArr[0].right, tempArr[1].right])
-      }
       tempArr[0].val += tempArr[1].val
     }
-  } 
+    if (tempArr[0].left !== null) {
+      queue.push([tempArr[0].left, tempArr[1].left])
+    } else {
+      tempArr[0].left = tempArr[1].left
+    }
+    if (tempArr[0].right !== null) {
+      queue.push([tempArr[0].right, tempArr[1].right])
+    } else {
+      tempArr[0].right = tempArr[1].right
+    }
+  }
 }
 
 
-let mergeTrees3 = (t1, t2) => {
-  // get all node's values of t2
-  if (t2 === null) {
-    return
-  } else {
-    let queue2 = [t2], arr2Val = [], node
-    while (queue2.length !== 0) {
-      node = queue2.shift()
-      if (node === null) {
-        arr2Val.push(0)
-        // direct to next loop
-        continue
-      } else {
-        arr2Val.push(node.val)
-      }
-      if (node.left !== null || node.right !== null) {
-        queue2.push(node.left)
-        queue2.push(node.right)
-      }
-    }
-  }
 
-  if (t1 === null) {
-    t1 = new TreeNode(t2.val)
-    return
-  } else {
-    let queue1 = [t1], arr1 = [arr2Val.shift()]
-    while (arr2Val.length !== 0) {
-      node = queue1.shift()
-      node.val += arr1.shift()
-      if (node.left !== null) {
-        queue1.push(node.left)
-        arr1.push(arr2Val.shift())
-      } else {
-        node.left = new TreeNode(arr2Val.shift())
-      }
-      if (node.right !== null) {
-        queue1.push(node.right)
-        arr1.push(arr2Val.shift())
-      } else {
-        node.right = new TreeNode(arr2Val.shift())
-      }
+
+// Example 
+
+let createTree = (arr) => {
+  let queue1 = [...arr]
+  let head  = new TreeNode(queue1.shift())
+  let queue2 = [head]
+  let temp1, temp2
+  while(true) {
+    temp2 = queue2.shift() 
+
+    temp1 = queue1.shift()
+    if (temp1 !== null) {
+      temp2.left = new TreeNode(temp1)
+      queue2.push(temp2.left)
     }
+    if (queue1.length === 0) return head
+
+    temp1 = queue1.shift()
+    if (temp1 !== null) {
+      temp2.right = new TreeNode(temp1)
+      queue2.push(temp2.right)
+    }
+    if (queue1.length === 0) return head
   }
 }
