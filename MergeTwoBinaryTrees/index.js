0f4f3f0748dbd22a3ceda30b022fa9b24cc3e104
Decolo <decolo@163.com>
2017-12-16 22:02:40 +0800

    'mergeBinaryTree fix'

--- MergeTwoBinaryTrees/index.js
+++ MergeTwoBinaryTrees/index.js
@@ -6,6 +6,38 @@ class TreeNode {
     this.left = this.right = null
   }
 }
+let createTree = (arr) => {
+  let queue1 = [...arr]
+  let head  = new TreeNode(queue1.shift())
+  let queue2 = [head]
+  let temp1, temp2
+  while(true) {
+    temp2 = queue2.shift()
+    if (temp2 === null) {
+      queue2.push(queue1.shift())
+      queue2.push(queue1.shift())
+      continue
+    }
+
+    temp1 = queue1.shift()
+    if (temp1 !== null) {
+      temp2.left = new TreeNode(temp1)
+      queue2.push(temp2.left)
+    } else {
+      queue2.push(null)
+    }
+    if (queue1.length === 0) return head
+
+    temp1 = queue1.shift()
+    if (temp1 !== null) {
+      temp2.right = new TreeNode(temp1)
+      queue2.push(temp2.right)
+    } else {
+      queue2.push(null)
+    }
+    if (queue1.length === 0) return head
+  }
+}
 /**
  * @param {TreeNode} t1
  * @param {TreeNode} t2
@@ -18,34 +50,36 @@ class TreeNode {
 
 
 // recursion 
-// let mergeToNewTree = (t1, t2) => {
-//   // init
-//   if (t1 === null && t2 === null) {
-//     return 
-//   } else if (t1 === null && t2 !== null) {
-//     return new TreeNode(t2.val)
-//   } else if (t1 !== null && t2 === null) {
-//     return new TreeNode(t1.val)
-//   } else {
-//     let queue1 = [[t1, t2]]
-//     let queue2 = []
-//     let tempArr
-//     while (queue1.length !== 0) {
-//       tempArr = queue1.shift()
-      
-//       if (tempArr[0] !== null && tempArr[1] !== null) {
-//         queue2.push(tempArr[0].val + tempArr[1].val)
-//         if (tempArr[0].left !== null || tempArr[1].left !== null) {
-//           queue1.push([tempArr[0].left, tempArr[1].left])
-//         }
-//         if (tempArr[0].right !== null || tempArr[1].right !== null) {
-//           queue1.push([tempArr[0].right, tempArr[1].right])
-//         }
-//       }
-//     }
-//     return t3
-//   }
-// }
+let mergeToNewTree = (t1, t2) => {
+  let queue = [[t1, t2]], tempArr, arr = []
+  while (queue.length !== 0) {
+    tempArr = queue.shift()
+    if (tempArr[0] !== null && tempArr[1] !== null) {
+      queue.push([tempArr[0].left, tempArr[1].left])
+      queue.push([tempArr[0].right, tempArr[1].right])
+      arr.push(tempArr[0].val + tempArr[1].val)
+    } else if (tempArr[0] !== null && tempArr[1] === null) {
+      queue.push([tempArr[0].left, null])
+      queue.push([tempArr[0].right, null])
+      arr.push(tempArr[0].val)
+    } else if (tempArr[0] === null && tempArr[1] !== null) {
+      queue.push([null, tempArr[1].left])
+      queue.push([null, tempArr[1].right])
+      arr.push(tempArr[1].val)
+    } else {
+      arr.push(null)
+    }
+  }
+
+  let i = arr.length - 1
+  while(i >= 0) {
+    if (arr[i]) break
+    i--
+  }
+  arr.length = i + 1
+
+  return createTree(arr)
+}
 
 
 
@@ -101,35 +135,3 @@ let mergeTrees2 = (t1, t2) => {
 
 // Example 
 
-// let createTree = (arr) => {
-//   let queue1 = [...arr]
-//   let head  = new TreeNode(queue1.shift())
-//   let queue2 = [head]
-//   let temp1, temp2
-//   while(true) {
-//     temp2 = queue2.shift()
-//     if (temp2 === null) {
-//       queue1.shift()
-//       queue1.shift()
-//       continue
-//     }
-
-//     temp1 = queue1.shift()
-//     if (temp1 !== null) {
-//       temp2.left = new TreeNode(temp1)
-//       queue2.push(temp2.left)
-//     } else {
-//       queue2.push(null)
-//     }
-//     if (queue1.length === 0) return head
-
-//     temp1 = queue1.shift()
-//     if (temp1 !== null) {
-//       temp2.right = new TreeNode(temp1)
-//       queue2.push(temp2.right)
-//     } else {
-//       queue2.push(null)
-//     }
-//     if (queue1.length === 0) return head
-//   }
-// }
